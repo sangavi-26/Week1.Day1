488f4e26abca9c0d51466d739696238449d8c15c
sangavi-26 <sangi.kings@gmail.com>
2022-03-23 14:24:29 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class PositiveNumber {
+
+	public static void main(String[] args) {
+		int number=-40;
+		if(number<0)
+		{
+			System.out.println("Number is a negative number");
+			number = number*-1;
+			System.out.println("The positive number is:" +number);
+		}
+
+	}
+
+}
