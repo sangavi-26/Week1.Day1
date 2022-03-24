2046dcaf37bc157b57e049021ed4ae94f2ea5f70
sangavi-26 <sangi.kings@gmail.com>
2022-03-24 11:57:50 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		int number=35;
+		if(number>0)
+		{
+			System.out.println("The number is positive");
+		}
+		else if(number<0)
+		{
+			System.out.println("The number is negative");
+		}
+		else if(number==0)
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+
+	}
+
+}
